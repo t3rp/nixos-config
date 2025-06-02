034578cf8c0674483e47661f1816d58eb25d32aa
t3rp <190659213+t3rp@users.noreply.github.com>
2025-06-02 13:29:35 -0400

    Added old VSCODE option

--- users/modules/vscode.nix
+++ users/modules/vscode.nix
@@ -13,26 +13,19 @@ in
   # Only enable VSCode if not in CI
   programs.vscode = lib.mkIf (!isCI) {
     enable = true;
-    
-    # NEW: Use profiles instead of direct extensions/userSettings
-    profiles.default = {
-      extensions = with pkgs.vscode-extensions; [
-        ms-python.python
-        ms-vscode.cpptools
-        bbenoist.nix
-        ms-python.python # Python support
-        redhat.vscode-yaml # YAML support
-        ms-vscode.cpptools # C/C++ support
-        ms-vscode.cmake-tools # CMake support
-        ms-vscode.makefile-tools # Makefile support
-        golang.go # Go support
-        rust-lang.rust-analyzer # Rust support
-        jnoortheen.nix-ide # Nix IDE support
-      ];
-      
-      userSettings = {
-        "editor.rulers" = [ 80 120 ];
-      };
+    extensions = with pkgs.vscode-extensions; [
+      ms-python.python
+      ms-vscode.cpptools
+      bbenoist.nix
+      redhat.vscode-yaml
+      ms-vscode.cmake-tools
+      ms-vscode.makefile-tools
+      golang.go
+      rust-lang.rust-analyzer
+      jnoortheen.nix-ide
+    ];
+    userSettings = {
+      "editor.rulers" = [ 80 120 ];
     };
   };
 }
