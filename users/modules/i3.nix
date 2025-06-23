5a4bade43f978e2d85e0bd4bcc56eab2508e0bae
t3rp <190659213+t3rp@users.noreply.github.com>
2025-06-23 14:11:10 -0400

    Mod and return alacritty i3

--- users/modules/i3.nix
+++ users/modules/i3.nix
@@ -48,7 +48,6 @@
       config = {
         # Basic settings
         modifier = "Mod4";  # Super key
-        # terminal = "${pkgs.rxvt-unicode}/bin/urxvt";
         window = {
           border = 1;
           hideEdgeBorders = "smart";
@@ -62,7 +61,8 @@
           mod = "Mod4";
         in {
           # Application launchers
-          "${mod}+Return" = "exec ${pkgs.rxvt-unicode}/bin/urxvt";
+          # "${mod}+Return" = "exec ${pkgs.rxvt-unicode}/bin/urxvt";
+          "${mod}+Return = "exec ${pkgs.alacritty}/bin/alacritty";
           "${mod}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
           "${mod}+Shift+Return" = "exec ${pkgs.firefox}/bin/firefox";
           "${mod}+e" = "exec ${pkgs.nemo}/bin/nemo";
