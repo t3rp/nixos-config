4b43de0523a46294234dc28f6119992eccda8074
t3rp <190659213+t3rp@users.noreply.github.com>
2025-06-24 09:36:02 -0400

    Moved i3 configuration to normal dotfile

--- users/modules/i3.nix
+++ users/modules/i3.nix
@@ -20,9 +20,9 @@
     rxvt-unicode
   ];
 
-  # Copy i3 configuration
+  # Copy dotfiles
   home.file = {
-    # Sway configuration
+    # i3 configuration
     ".config/i3/config".source = ../dotfiles/i3/config;
   };
 
@@ -46,49 +46,6 @@
       export XDG_SESSION_TYPE=x11
       export XDG_CURRENT_DESKTOP=i3
     '';
-
-    # i3
-    windowManager.i3 = {
-      enable = true;
-
-      config = {
-        # Basic settings
-        modifier = "Mod4";  # Super key
-        window = {
-          border = 1;
-          hideEdgeBorders = "smart";
-        };
-
-        # Workspace settings
-        workspaceAutoBackAndForth = true;
-
-        # Key bindings
-        keybindings = let
-          mod = "Mod4";
-        in {
-          # Screen lock
-          "${mod}+Ctrl+l" = "exec ${pkgs.i3lock}/bin/i3lock -c 000000";
-
-          # Volume controls
-          "XF86AudioRaiseVolume" = "exec ${pkgs.pamixer}/bin/pamixer -i 5";
-          "XF86AudioLowerVolume" = "exec ${pkgs.pamixer}/bin/pamixer -d 5";
-          "XF86AudioMute" = "exec ${pkgs.pamixer}/bin/pamixer -t";
-
-          # Brightness controls
-          "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set +10%";
-          "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 10%-";
-
-          # Screenshot - full screen
-          "Print" = "exec ${pkgs.scrot}/bin/scrot ~/Pictures/screenshot_%Y%m%d_%H%M%S.png && notify-send 'Screenshot saved'";
-          
-          # Screenshot - select area
-          "${mod}+Print" = "exec ${pkgs.scrot}/bin/scrot -s ~/Pictures/screenshot_%Y%m%d_%H%M%S.png";
-          
-          # Screenshot - current window
-          "${mod}+Shift+Print" = "exec ${pkgs.scrot}/bin/scrot -u ~/Pictures/screenshot_%Y%m%d_%H%M%S.png";
-        };
-      };
-    };
   };
 
   # Rofi configuration
