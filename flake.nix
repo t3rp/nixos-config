9c8c00c2f4efaf11fd5067fdd2c06b6d26683845
t3rp <190659213+t3rp@users.noreply.github.com>
2025-07-18 11:19:50 -0400

    Added Athena flake

--- flake.nix
+++ flake.nix
@@ -29,6 +29,21 @@
       };
     };
 
+      # === ATHENA ===
+      athena = let
+        username = "terp";
+        specialArgs = {inherit username;};
+      in nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/ares/configuration.nix
+          ./hosts/modules/sway.nix
+          ./hosts/modules/libvirtd.nix
+        ];
+      };
+    };
+
     # Standalone Home Manager configurations
     homeConfigurations = {
       # NixOS user (for standalone use)
